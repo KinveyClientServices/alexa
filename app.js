5cb4a773d45d21aacc6232284006f2f834d92780
Ivo Janssen <ivo@kinvey.com>
2017-06-08 20:53:40 -0500

    look up user via /validate route in KAS to get username

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var Kinvey = require('kinvey-node-sdk');
 var app = express();
 app.use(bodyParser.json());
 
+var michostname = "https://auth.kinvey.com";
 var kid = process.env.kid || "kid_rkRcPjfhe";
 var masterSecret = process.env.master_secret || "";
 
@@ -15,33 +16,40 @@ app.post("/skilltest", function(req,res){
   console.log(req.body);
 
   // step 1: validate user
-  // (right now we just log in as "master" in the initialize function)
-  if request.session,access_token......
+  var username = "";
+  var token = req.body.session.user.accessToken;
 
-  // step 2: act on the intent
-  // if (req.body.request.intent.name === "GetBooks" ) {
+  validateUser(token, (err,user) => {
+    if (user.id) {
+      username = user.id
+    }
 
-  var outputSpeech = "Hi there, the list of books is ";
-  Kinvey.DataStore.collection('patient',Kinvey.DataStoreType.Network).find().subscribe(function onNext(books) {
-    console.log("FOUND BOOKS:");
-    books.forEach(function (book) {
-      console.log("- "+book.title);
-      outputSpeech += book.title+", ";
-    });
-    console.log("END OF LIST");
-    var response = { version: "1.0",
-                     response:{
-                       outputSpeech: {
-                         type: "PlainText",
+    // step 2: act on the intent
+    // if (req.body.request.intent.name === "GetBooks" ) {
+
+    var outputSpeech = "Hi there "+username+", the list of books is ";
+    Kinvey.DataStore.collection('books',Kinvey.DataStoreType.Network).find().subscribe(function onNext(books) {
+      console.log("FOUND BOOKS:");
+      books.forEach(function (book) {
+        console.log("- "+book.title);
+        outputSpeech += book.title+", ";
+      });
+      console.log("END OF LIST");
+      var response = { version: "1.0",
+                       response:{
+                         outputSpeech: {
+                           type: "PlainText",
+                         }
                        }
-                     }
-                   };
-    response.response.outputSpeech.text = outputSpeech;
-    console.log(response);
-    return res.status(200).send(response);
+                     };
+      response.response.outputSpeech.text = outputSpeech;
+      console.log(response);
+      return res.status(200).send(response);
+    });
   });
 });
 
+// INITIALIZE KINVEY SDK
 Kinvey.initialize({
     appKey:      kid,
     appSecret:   masterSecret
@@ -57,3 +65,23 @@ Kinvey.initialize({
   var port = process.env.PORT||3001;
   app.listen(port, function(){console.log("listening on :"+port)});
 });
+
+// EXCHANGES KINVEY KAS ACCESS TOKEN FOR USER INFORMATION
+function validateUser(token, cb) {
+  if (token === null) {
+    cb(null,{});
+  }
+  var options = {
+    url: michostname+"/v3/oauth/validate?access_token="+token,
+    json: true
+  };
+  request(options, (error, response, resBody) => {
+    if (!error && response.statusCode == 200) {
+      return cb(null,resBody);
+    } else if (!error && response.statusCode != 200) {
+      return cb(resBody,null);
+    } else {
+      return cb(error,null);
+    }
+  });
+}
